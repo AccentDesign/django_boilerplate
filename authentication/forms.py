1d9554d6291b44d115004afa758ab2ad3ed560b8
Stuart George <stuart.bigmassa@gmail.com>
2016-08-04 21:33:51 +0100

    moved auth forms to forms module

---
+++ authentication/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from authentication.models import User
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
