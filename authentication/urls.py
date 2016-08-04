80ba94838b659fe9658182165e62558d51d4f76d
Stuart George <stuart.bigmassa@gmail.com>
2016-08-04 21:28:43 +0100

    auth urls more readable and overrode auth login template

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,26 +1,39 @@
 from django.conf.urls import url
-from django.contrib.auth.views import (
-    login,
-    logout,
-    password_change,
-    password_change_done,
-    password_reset,
-    password_reset_done,
-    password_reset_confirm,
-    password_reset_complete
-)
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
-    url(r'^password_change/$', password_change, name='password_change'),
-    url(r'^password_change/done/$', password_change_done, name='password_change_done'),
+    url(r'^login/$',
+        auth_views.login,
+        name='login'),
+
+    url(r'^logout/$',
+        auth_views.logout,
+        {'next_page': '/'},
+        name='logout'),
+
+    url(r'^password_change/$',
+        auth_views.password_change,
+        name='password_change'),
+
+    url(r'^password_change/done/$',
+        auth_views.password_change_done,
+        name='password_change_done'),
+
     url(r'^password_reset/$',
-        password_reset,
+        auth_views.password_reset,
         {'html_email_template_name': 'emails/password_reset_email.html'},
         name='admin_password_reset'),
-    url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<token>.+)-(?P<uidb64>[0-9A-Za-z]+)/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
+
+    url(r'^password_reset/done/$',
+        auth_views.password_reset_done,
+        name='password_reset_done'),
+
+    url(r'^reset/(?P<token>.+)-(?P<uidb64>[0-9A-Za-z]+)/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+
+    url(r'^reset/done/$',
+        auth_views.password_reset_complete,
+        name='password_reset_complete'),
 ]
