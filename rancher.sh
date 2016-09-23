ff0ce9e49fcb2620a62137e2c94ac84af95bf173
Stuart George <stuart@accentdesign.co.uk>
2016-09-23 16:01:16 +0100

    added sleep for rancher db host

--- rancher.sh
+++ rancher.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+echo "waiting for db..."
+while ! nc -w 1 -z $RDS_HOSTNAME $RDS_PORT 2>/dev/null;
+do
+  echo -n .
+  sleep 1
+done
+echo "db ready"
+
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
