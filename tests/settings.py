858170db0c7bb53dd4a100e4aa4efe7ed5d455fd
Stuart George <stuart@accentdesign.co.uk>
2018-08-14 09:30:35 +0100

    circleci2

--- tests/settings.py
+++ tests/settings.py
@@ -3,11 +3,4 @@ from app.settings import *
 
 SECRET_KEY = 'secret'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = []
